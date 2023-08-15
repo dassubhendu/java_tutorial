62a3e8a3371126c205a47a95ebd926efe6f75e39
Subhendu Das <subhendudas@Subhendus-MacBook-Pro.local>
2023-08-15 13:26:06 +0530

    Question Set

---
+++ src/main/java/ClassObject/ChildClass.java
@@ -0,0 +1,13 @@
+package ClassObject;
+
+public class ChildClass {
+
+//    public void test(){
+//        System.out.println("This is Selenium test!!");
+//    }
+
+    public static void test(){
+        System.out.println("This is Selenium test!!");
+    }
+
+}
---
+++ src/main/java/ClassObject/MainClass.java
@@ -0,0 +1,14 @@
+package ClassObject;
+
+public class MainClass {
+    public static void main(String args[]){
+        /**
+         *Creating object of ChildClass
+         */
+//        ChildClass c1 = new ChildClass();
+//        c1.test();
+
+        ChildClass.test();
+
+    }
+}
